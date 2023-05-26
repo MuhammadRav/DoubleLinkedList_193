ea62f4aada237590fe9a9b22968400fe7c5b11e0
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-26 15:54:25 +0700

    da

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -39,4 +39,18 @@ void DoubleLinkedList::addNode() {
 	Node* newNode = new Node();		//step 1
 	newNode->noMhs = nim;		//step 2
 	newNode->name = nama;		//step 2
+
+	/*insert a node in the beginning of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;		//step 3
+		if (START != NULL)
+			START->prev = newNode;		//step 4
+		newNode->prev = NULL;		//step 5
+		START = newNode;		//step 6
+		return;
+	}
 }
